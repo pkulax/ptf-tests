b9b4c0834083b4a515104c7e70e714c672ee498f
Kula, PrzemyslawX <przemyslawx.kula@intel.com>
2023-03-31 20:19:09 +0200

    [storage] Add posibility run tests from one machine to another and add socket terminal
    Signed-off-by: Kula, PrzemyslawX <przemyslawx.kula@intel.com>

--- storage/tests/system_tools/vm.py
+++ storage/tests/system_tools/vm.py
@@ -11,7 +11,7 @@ from tenacity import retry, stop_after_delay
 from system_tools.const import (DEFAULT_HOST_TARGET_SERVICE_PORT_IN_VM,
                                 DEFAULT_QMP_PORT)
 from system_tools.errors import BusyPortException, VirtualizationException
-from system_tools.socket_functions import send_command_over_unix_socket
+from system_tools.terminals import UnixSocketTerminal
 
 
 class VirtualMachine:
@@ -27,12 +27,13 @@ class VirtualMachine:
         self.socket_path = os.path.join(
             self.platform.terminal.config.vm_share_dir_path, "vm_socket"
         )
+        self.socket_terminal = UnixSocketTerminal(self.platform.terminal, self.socket_path)
 
     def run(self, login, password):
         self.delete()
         if self.platform.get_pid_from_port(DEFAULT_QMP_PORT):
             raise BusyPortException(f"There is process in {DEFAULT_QMP_PORT} port")
-        if os.path.exists(self.socket_path):
+        if self.platform.path_exist(self.socket_path):
             raise VirtualizationException("Socket path is not free")
         cmd = f"SHARED_VOLUME={self.platform.terminal.config.vm_share_dir_path} UNIX_SERIAL=vm_socket scripts/vm/run_vm.sh &> /dev/null &"
         self.platform.terminal.execute(f"cd {self.storage_path} && {cmd}")
@@ -51,20 +52,17 @@ class VirtualMachine:
     @retry(stop=stop_after_delay(600), reraise=True)
     def _wait_to_run(self, port):
         time.sleep(30)
-        if not self.platform.get_pid_from_port(port) or not os.path.exists(
+        if not self.platform.get_pid_from_port(port) or not self.platform.path_exist(
             self.socket_path
         ):
             raise VirtualizationException("VM is not running")
         time.sleep(60)
 
     def _login(self, login, password):
-        # TODO create UnixSocketTerminal
-        send_command_over_unix_socket(self.socket_path, login, 1)
-        send_command_over_unix_socket(self.socket_path, password, 1)
+        self.socket_terminal.execute(login, 1)
+        self.socket_terminal.execute(password, 1)
 
     def get_number_of_virtio_blk_devices(self):
         cmd = "ls -1 /dev"
-        out = send_command_over_unix_socket(
-            sock=self.socket_path, cmd=cmd, wait_for_secs=1
-        )
+        out = self.socket_terminal.execute(cmd, 1)
         return len(re.findall("vd[a-z]+\\b", out))
